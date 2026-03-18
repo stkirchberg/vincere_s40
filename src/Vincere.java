c2fee2b887050c186554a1f89821916b41c6cb1d
stkirchberg <s.t.kirchberg@web.de>
2026-03-18 13:36:04 +0100

    works and looks good

--- src/Vincere.java
+++ src/Vincere.java
@@ -4,66 +4,91 @@ import java.util.Vector;
 
 public class Vincere extends MIDlet implements CommandListener {
     private Display display;
-
-    private Form loginForm;
-    private Form chatForm;
-
-    private TextField tfUser, tfPass, tfInput;
-    private Command cmdLogin, cmdSend, cmdExit;
-
-    private Vector chatMessages = new Vector();
+    private ChatCanvas canvas;
+    private TextBox inputBuffer;
+    private Command sendCommand, exitCommand;
+    private String userName = "User"; 
 
     public Vincere() {
         display = Display.getDisplay(this);
-
-        loginForm = new Form("vincere Login");
-        tfUser = new TextField("Nutzer:", "", 15, TextField.ANY);
-        tfPass = new TextField("Passwort:", "", 15, TextField.PASSWORD);
-        cmdLogin = new Command("Login", Command.OK, 1);
-        cmdExit = new Command("Exit", Command.EXIT, 1);
-        
-        loginForm.append(tfUser);
-        loginForm.append(tfPass);
-        loginForm.addCommand(cmdLogin);
-        loginForm.addCommand(cmdExit);
-        loginForm.setCommandListener(this);
+        canvas = new ChatCanvas();
+        inputBuffer = new TextBox("Nachricht", "", 100, TextField.ANY);
+        sendCommand = new Command("Senden", Command.OK, 1);
+        exitCommand = new Command("Abbrechen", Command.BACK, 2);
+        inputBuffer.addCommand(sendCommand);
+        inputBuffer.addCommand(exitCommand);
+        inputBuffer.setCommandListener(this);
     }
 
     protected void startApp() {
-        display.setCurrent(loginForm);
+        display.setCurrent(canvas);
     }
 
     public void commandAction(Command c, Displayable d) {
-        if (c == cmdExit) {
-            notifyDestroyed();
-        } else if (c == cmdLogin) {
-            startChat();
-        } else if (c == cmdSend) {
-            processMessage();
-        }
-    }
-
-    private void startChat() {
-        chatForm = new Form("vincere - " + tfUser.getString());
-        tfInput = new TextField("Nachricht:", "", 100, TextField.ANY);
-        cmdSend = new Command("Senden", Command.OK, 1);
-        
-        chatForm.addCommand(cmdSend);
-        chatForm.append(tfInput);
-        chatForm.setCommandListener(this);
-        display.setCurrent(chatForm);
-    }
-
-    private void processMessage() {
-        String msg = tfInput.getString();
-        if (msg.length() > 0) {
-            chatMessages.addElement(tfUser.getString() + ": " + msg);
-
-            chatForm.insert(0, new StringItem(null, "> " + msg));
-            tfInput.setString("");
+        if (c == sendCommand) {
+            String msg = inputBuffer.getString();
+            if (msg.length() > 0) {
+                canvas.addMessage(userName, msg);
+                inputBuffer.setString("");
+            }
+            display.setCurrent(canvas);
+        } else if (c == exitCommand) {
+            display.setCurrent(canvas);
         }
     }
 
     protected void pauseApp() {}
     protected void destroyApp(boolean unconditional) {}
+
+    class ChatCanvas extends Canvas {
+        private Vector messages = new Vector();
+        private int bgColor = 0x000000;
+        private int matrixGreen = 0x00FF00;
+        private int userColor = 0x00AA00;
+
+        public void addMessage(String user, String text) {
+            messages.addElement(user + ": " + text);
+            if (messages.size() > 15) {
+                messages.removeElementAt(0);
+            }
+            repaint();
+        }
+
+        protected void paint(Graphics g) {
+            int w = getWidth();
+            int h = getHeight();
+
+            g.setColor(bgColor);
+            g.fillRect(0, 0, w, h);
+
+            g.setColor(matrixGreen);
+            g.drawLine(0, 25, w, 25);
+            g.setFont(Font.getFont(Font.FACE_SYSTEM, Font.STYLE_BOLD, Font.SIZE_SMALL));
+            g.drawString("VINCERE // PUBLIC_ROOM", 5, 5, Graphics.TOP | Graphics.LEFT);
+
+            int y = h - 40;
+            g.setFont(Font.getFont(Font.FACE_MONOSPACE, Font.STYLE_PLAIN, Font.SIZE_SMALL));
+            
+            for (int i = messages.size() - 1; i >= 0; i--) {
+                String m = (String) messages.elementAt(i);
+                if (m.startsWith(userName)) {
+                    g.setColor(0xFFFFFF);
+                } else {
+                    g.setColor(matrixGreen);
+                }
+                g.drawString(m, 5, y, Graphics.TOP | Graphics.LEFT);
+                y -= 18;
+                if (y < 30) break;
+            }
+
+            g.setColor(userColor);
+            g.fillRect(0, h - 20, w, 20);
+            g.setColor(0x000000);
+            g.drawString("Druecke Taste zum Tippen...", 5, h - 18, Graphics.TOP | Graphics.LEFT);
+        }
+
+        protected void keyPressed(int keyCode) {
+            display.setCurrent(inputBuffer);
+        }
+    }
 }
